5a0e68df9e985c3843c49e4db9af38a92596c792
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-21 10:20:49 +0500

    swagger added

---
+++ src/main/java/uz/everbest/auth/config/OpenApi30Config.java
@@ -0,0 +1,43 @@
+package uz.everbest.auth.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApi30Config {
+    private final String moduleName;
+    private final String apiVersion;
+
+    public OpenApi30Config(
+            @Value("${app.module-name}") String moduleName,
+            @Value("${app.api-version}") String apiVersion) {
+        this.moduleName = moduleName;
+        this.apiVersion = apiVersion;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", moduleName);
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info().title(apiTitle).version(apiVersion));
+    }
+
+}
