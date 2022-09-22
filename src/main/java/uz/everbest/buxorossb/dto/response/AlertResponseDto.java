9175bd6d46fb82152b59eea224a721abb41f5918
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-22 12:00:05 +0500

    success true without message

--- src/main/java/uz/everbest/buxorossb/dto/response/AlertResponseDto.java
+++ src/main/java/uz/everbest/buxorossb/dto/response/AlertResponseDto.java
@@ -1,5 +1,6 @@
 package uz.everbest.buxorossb.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AlertResponseDto {
     private String message;
     private boolean success;
